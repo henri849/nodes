9ded646da41626863912d0c29b2b0ea2b7325bb0
kehillbruh <kehillahrobot@gmail.com>
2024-04-24 22:43:45 +0200

    added some rotation tracking functionalities

--- Motor2.py
+++ Motor2.py
@@ -4,9 +4,6 @@ from rclpy.node import Node
 from std_msgs.msg import Float32
 #GPIO imports
 import Jetson.GPIO as GPIO
-#System imports
-import os 
-import time
 #Unit system
 import Unit
 
@@ -14,16 +11,41 @@ import Unit
 class Motor(Node):
     def __init__(self, name: str, enable_pin: int, A_pin: int, B_pin: int, input_pin: int):
         super().__init__(name)# ROS2 node initialization
-        #Saving pins pins
+        #Saving pins
         self.name = name
         self.enable = enable_pin  #turns the motor on or off
         self.A = A_pin #Serves to set different angular velocities
         self.B = B_pin #Serves to set different angular velocities
         self.inp = input_pin # gives pulses every delta_pulse degrees
 
-        self.GPIO_Setup()#GPIO - sets up all input-output functionallity
+        #GPIO - sets up all input-output functionallity
+        self.GPIO_Setup()
+
+        #Internal variables for controlling
+        self.angle = Unit.Angle(0,"degrees") # current motor angle
+        self.TargetAngle = Unit.Angle(0,"degrees") # angle the motor targets
+        self.rates = {0:0,1:-100,2:100,3:-4000,4:4000} # different rates in rotations/minute
+        self.last_gear = 0 # last speed setting
+        self.gear =  0# current speed setting
+        self.pulse_delta = Unit.Angle(360/16,"degrees") # how often the motors get a pulse, 16ppr
+
+        #Rotation detection
+        GPIO.add_event_detect(self.inp, GPIO.RISING, callback = self.pulse) # event detector for motor pulse
 
         self.Close()
+
+    def pulse(self, channel):
+        if self.rates[self.gear]>0: #if we are rotating CCW
+            self.angle += Unit.Angle(self.pulse_delta,"degrees")
+        if self.rates[self.gear]<0:#if we are rotating CW
+            self.angle -= Unit.Angle(self.pulse_delta,"degrees")
+            
+        if self.rates[self.gear] == 0:# if our momentum carried us over when we stopped the motor
+            self.get_logger().warn(f"{self.name} received pulse when gear=0! defaulting to last year setting")
+            if self.rates[self.last_gear]>0: #if we are rotating CCW
+                self.angle += Unit.Angle(self.pulse_delta,"degrees")
+            if self.rates[self.last_gear]<0:#if we are rotating CW
+                self.angle -= Unit.Angle(self.pulse_delta,"degrees")
     
     def GPIO_Setup(self):
         #board mode
@@ -44,8 +66,8 @@ class Motor(Node):
         
 
 if __name__ == "__main__":
-    rclpy.init()
-    m1 = Motor(name = "Winch", enable_pin = 19, A_pin = 15, B_pin = 13, input_pin = 11)
-    rclpy.spin(m1)
-    m1.destroy_node()
-    rclpy.shutdown()
+    rclpy.init()# Starts ROS2
+    m1 = Motor(name = "Winch", enable_pin = 19, A_pin = 15, B_pin = 13, input_pin = 11)#Creates Winch object
+    rclpy.spin(m1)# Launches Winch
+    m1.destroy_node()# Destroys Winch once it's done running
+    rclpy.shutdown()# Turns ROS2 Off
