9d04078dfc0016183de17fcde7f3e42bd5b4d1e2
kehillbruh <kehillahrobot@gmail.com>
2024-04-24 22:15:29 +0200

    Added imports for cleaned up motor class

---
+++ Motor2.py
@@ -0,0 +1,12 @@
+#ROS2 imports
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32
+#GPIO imports
+import Jetson.GPIO as GPIO
+#System imports
+import os 
+import time
+#Unit system
+import Unit
+
