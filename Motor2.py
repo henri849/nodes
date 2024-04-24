c1ec04b6be4decca2ffd9d61c0d9727bf498c245
kehillbruh <kehillahrobot@gmail.com>
2024-04-24 22:21:45 +0200

    Wrote the motor class and created an instance

--- Motor2.py
+++ Motor2.py
@@ -10,3 +10,11 @@ import time
 #Unit system
 import Unit
 
+
+class Motor(Node):
+    def __init__(self, name: str, enable_pin: int, A_pin: int, B_pin: int, input_pin: int):
+        super().__init__(name)# ROS2 node initialization
+        
+
+if __name__ == "__main__":
+    m1 = Motor("Winch", enable_pin = 19, A_pin = 15, B_pin = 13, input_pin = 11)
