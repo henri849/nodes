cab06fbd3e099b2c7043e9a1d048723cdd95b229
kehillbruh <kehillahrobot@gmail.com>
2024-04-29 00:06:31 +0200

    Created new cleaned up windsensor class

--- WindSensor.py
+++ WindSensor.py
@@ -5,6 +5,7 @@ import board
 import adafruit_ads7830.ads7830 as ADC
 from adafruit_ads7830.analog_in import AnalogIn
 import time
+
 class WindSensor(Node):
     def __init__(self, chan):
         super().__init__('WindSensor')
@@ -39,3 +40,18 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# import time
+# import board
+# import adafruit_ads7830.ads7830 as ADC
+# from adafruit_ads7830.analog_in import AnalogIn
+
+# i2c = board.I2C()
+
+# # Initialize ADS7830
+# adc = ADC.ADS7830(i2c)
+# chan = AnalogIn(adc, 3)
+
+# while True:
+#     print(f"ADC channel 3 = {5/(2**16)*chan.value}, raw value:{chan.value}")
+#     time.sleep(0.1)
