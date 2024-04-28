9dc19cfa3d28c99c75fec7eaf5f6bb1261f04fbe
kehillbruh <kehillahrobot@gmail.com>
2024-04-29 00:07:05 +0200

    Cleaned up radio class

--- Radio.py
+++ Radio.py
@@ -1,56 +1,30 @@
 import rclpy
 import serial
 from rclpy.node import Node
-from std_msgs.msg import Float32
-import math
+from std_msgs.msg import Float32MultiArray
 import time
+
 class Radio(Node):
     def __init__(self, ser):
-        super().__init__('radio')
-        self.publisher_ = self.create_publisher(Float32, 'radio', 5)
-        self.ser = ser
-        self.time_ping = time.time()
-        self.active = True
-        self.publisher()
-
-    def publisher(self):
-        msg = Float32()
-        while True:
-            if self.ser.in_waiting >0:
-                inp = self.ser.readline()[:-1] #ord(self.ser.read())
-                print(inp,str(inp)[2:-1].split(",")[0])
-                msg.data = float(str(inp)[2:-1].split(",")[0])#int.from_bytes(inp, "little") 
-                self.publisher_.publish(msg)
-                self.get_logger().info('radio: ' + str(msg.data))
-                # print(inp, int.from_bytes(inp, "little") )
-                # inp = self.ser.read() #ord(self.ser.read()) 
-                # print(inp, ord(inp))
-                # print(inp, int.from_bytes(inp, "little") )
-                # msg.data = ord(inp)
-                # if inp == 113:#q
-                #     self.ser.write("OK\n".encode("utf-8"))
-                #     if not self.active:
-                #         self.get_logger().warning("Radio reconnected after {} seconds".format(int(time.time() -self.time_ping)))
-                #         self.active = True
-                #     self.time_ping = time.time()
-                # else:
-            # elif time.time() -self.time_ping > 20 and self.active:
-            #     self.get_logger().warning("No radio pings received in : {} seconds".format(int(time.time() -self.time_ping)))
-            #     self.active = False
-                # self.radioReset()
-
-    def radioReset(self):
-        self.ser.write(bytes('+++', 'utf-8'))
-        time.sleep(1)
-        self.ser.write(bytes('ATB\n\r', 'utf-8'))
-        time.sleep(1)
-        self.ser.write(bytes('ATO\n\r', 'utf-8'))
-        self.get_logger().info('radio reset complete')
-        self.time_ping = time.time()
-
+        super().__init__('radio')#Creating new node with name "radio"
+        self.publisher_ = self.create_publisher(Float32MultiArray, 'controller', 5) #we'll proadcast what we get from the radio to other nodes through this topic called "controller"
+        self.ser = ser # serial object, this is how we read from our radio module 
+        self.publisher()# function that perpetually reads radio and broadcasts what it receives to other nodes
     
-def main():
-    ser = serial.Serial(port="/dev/ttyACM0", baudrate=57600)
+    def publisher(self):
+        inst = Float32MultiArray() # instructions are to be stored in here to be sent
+        while True:
+            if self.ser.in_waiting >0: # if the radio has received anything it will go into the radio's buffer, this checks if the buffer is not empty
+                inst.data = self.parse(self.ser.readline())# readline waits for a newline character "\n" then returns everythign before it including the \n 
+                self.get_logger().debug('radio: ' + str(inst.data))
+                self.publisher_.publish(inst)
+
+    def parse(self,msg):
+        msg = msg.split(b',')   
+        return list(map(lambda a: float(a),msg))
+    
+if __name__ == "__main__":
+    ser = serial.Serial(port="/dev/ttyACM0", baudrate=57600)#sudo chmod 666 /dev/ttyACM0
     rclpy.init()
     print("radio constructed")
     radio = Radio(ser)
@@ -60,6 +34,3 @@ def main():
     rclpy.shutdown()
     print("radio shutdown")
 
-
-if __name__ == "__main__":
-    main()
